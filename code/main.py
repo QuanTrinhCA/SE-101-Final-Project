c78e8b46af5e7102d09cc36e27dabb258536abaf
Quan Trinh <q2trinh@uwaterloo.ca>
2023-11-28 21:51:15 -0500

    Init communication between ui and backend

--- code/main.py
+++ code/main.py
@@ -1,8 +1,12 @@
 from multiprocessing import Process, Pipe
 from backend import audio_backend
 from emotiondection import emotion_detect
+from ui import ui
 
 if __name__ == '__main__':
+    conn_to_ui,conn_to_main = Pipe()
+    backend_process = Process(target=ui, args=(conn_to_main,))
+    backend_process.start()
     conn_to_audio_backend,conn_to_main = Pipe()
     backend_process = Process(target=audio_backend, args=(conn_to_main,))
     backend_process.start()
@@ -22,26 +26,30 @@ if __name__ == '__main__':
         # Receiving info
         if conn_to_audio_backend.poll():
             info = conn_to_audio_backend.recv()
+            sending_info = {}
             for key in info:
                 if (key == 'position'):
                     position = info[key]
                     if (position > 0.9):
                         conn_to_emotion_detection.send({'action': 'get_emotion'})
-                    # Send to UI
+                    sending_info['position'] = position
                 if (key == 'title'):
-                    print(info[key])
-                    # Send to UI
+                    sending_info['title'] = info[key]
                 if (key == 'thumbnailurl'):
-                    print(info[key])
-                    # Send to UI
+                    sending_info['thumbnailurl'] = info[key]
                 if (key == 'length'):
-                    print(info[key])
-                    # Send to UI
+                    sending_info['length'] = info[key]
                 if (key == 'artist'):
-                    print(info[key])
-                    # Send to UI
+                    sending_info['artist'] = info[key]
+                conn_to_ui.send(sending_info)
         if conn_to_emotion_detection.poll():
             info = conn_to_emotion_detection.recv()
             for key in info:
                 if (key == 'emotion'):
                     emotion = info[key]
+        if conn_to_ui.poll():
+            order = conn_to_ui.recv()
+            sending_info = {'action': order['action']}
+            if (order['action'] == 'set_volume'):
+                sending_info['volume'] = order['volume']
+            conn_to_audio_backend.send(sending_info)
--- code/ui.py
+++ code/ui.py
@@ -1,19 +1,14 @@
-import os
 import tkinter as tk
-from tkinter import filedialog
 from tkinter import ttk
 #from PIL import Image, ImageTk
-from pygame import mixer
 
-class MusicPlayer:
-    def __init__(self, root):
+class App:
+    def __init__(self, root, conn_to_main):
         self.root = root
-        self.root.title("Music Player")
-        self.root.geometry("400x250")
+        self.root.title("Smert")
+        self.root.geometry("800x480")
 
-        self.current_track = None
-        self.tracks = []
-        self.track_index = 0
+        self.conn_to_main = conn_to_main
 
         # Load the image
         #image = Image.open("music_icon.png")  # Replace "music_icon.png" with your image file
@@ -21,6 +16,21 @@ class MusicPlayer:
         #self.photo = ImageTk.PhotoImage(image)
 
         self.create_widgets()
+
+    def updateTitle(self, title):
+        self.song_name_label.config(text=title)
+
+    def updateProgress(self, progress):
+        return
+    
+    def updateVolume(self, volume):
+        self.volume_slider.config(value=volume)
+
+    def pauseAudio(self):
+        self.conn_to_main.send({'action': 'pause'})
+
+    def unpauseAudio(self):
+        self.conn_to_main.send({'action': 'unpause'})
 
     def create_widgets(self):
         # Display the image at the top
@@ -32,25 +42,47 @@ class MusicPlayer:
         self.song_name_label.pack(pady=30)
 
         # Create buttons
-        self.play_button = tk.Button(self.root, text="Play", command=self.play_track,width=8, padx=5, pady=5)
+        self.play_button = tk.Button(self.root, text="Play", command=self.unpauseAudio, width=8, padx=5, pady=5)
         self.play_button.pack(side=tk.LEFT, padx=5)
 
-        self.pause_button = tk.Button(self.root, text="Pause", command=self.pause_track, width=8, padx=5, pady=5)
+        self.pause_button = tk.Button(self.root, text="Pause", command=self.pauseAudio, width=8, padx=5, pady=5)
         self.pause_button.pack(side=tk.LEFT, padx=5)
 
-        self.next_button = tk.Button(self.root, text="Next", command=self.next_track, width=8, padx=5, pady=5)
+        self.next_button = tk.Button(self.root, text="Next", width=8, padx=5, pady=5)
         self.next_button.pack(side=tk.LEFT, padx=5)
 
-        self.like_button = tk.Button(self.root, text="Like", command=self.like_track, width=8, padx=5, pady=5)
+        self.like_button = tk.Button(self.root, text="Like", width=8, padx=5, pady=5)
         self.like_button.pack(side=tk.LEFT, padx=5)
 
-        self.dislike_button = tk.Button(self.root, text="Dislike", command=self.dislike_track, width=8, padx=5, pady=5)
+        self.dislike_button = tk.Button(self.root, text="Dislike", width=8, padx=5, pady=5)
         self.dislike_button.pack(side=tk.LEFT, padx=5)
 
-        self.volume_slider = ttk.Scale(self.root, from_=0, to=1, orient="vertical", command=self.set_volume,length=200)
+        self.volume_slider = ttk.Scale(self.root, from_=0, to=100, orient="vertical", length=200)
         self.volume_slider.set(0.5)
         self.volume_slider.pack(side=tk.RIGHT, padx=10, pady=7)
 
         # Create status label
         self.status_label = tk.Label(self.root, text="No track loaded.")
         self.status_label.pack(side=tk.BOTTOM, pady=10)
+
+def ui(conn_to_main):
+    root = tk.Tk()
+    mp = App(root=root, conn_to_main=conn_to_main)
+    root.update()
+    while True:
+        if conn_to_main.poll():
+            info = conn_to_main.recv()
+            for key in info:
+                if (key == 'position'):
+                    mp.updateProgress(progress=info[key])
+                if (key == 'title'):
+                    mp.updateTitle(title=info[key])
+                if (key == 'thumbnailurl'):
+                    print(info[key])
+                if (key == 'length'):
+                    print(info[key])
+                if (key == 'artist'):
+                    print(info[key])
+                if (key == 'volume'):
+                    mp.updateVolume(volume=info[key])
+        root.update()
